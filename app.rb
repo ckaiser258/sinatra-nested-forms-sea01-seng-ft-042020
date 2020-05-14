8ac0dbaad4ff4ca7f64ebf7ec73d395800e4e1cf
ckaiser258 <kaiserc2@seattleu.edu>
2020-05-14 09:54:40 -0700

    passed some more tests

--- app.rb
+++ app.rb
@@ -3,16 +3,21 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    get '/newpirate' do
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
       erb :'pirates/new'
     end
 
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
+      @ships = []
       params[:ships].each do |ship_hash|
-        Ship.new(ship_hash)
+        @ships << Ship.new(ship_hash)
       end
-      @ships = Ship.all
+      @ships
       erb :'pirates/show'
     end
 
